6da410c3ee3deb3ef0082f92461803384b39bec8
takuseno <takuma.seno@gmail.com>
2018-05-22 15:06:40 +0900

    Updates

--- src/reducers/dialogs/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import createFileDialog from './createFileDialog'
-
-export default combineReducers({
-  createFileDialog
-})
--- src/reducers/repositories.js
+++
@@ -1,9 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case 'RECEIVE_ORG_REPOSITORIES':
-    case 'RECEIVE_USER_REPOSITORIES':
-      return action.repos
-    default:
-      return state
-  }
-}
