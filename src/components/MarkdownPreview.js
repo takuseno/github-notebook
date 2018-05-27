5a0296c4fa799d41d1936e0207e64a9bd9245b10
takuseno <takuma.seno@gmail.com>
2018-05-27 11:28:07 +0900

    Enable tex rendering

--- src/components/MarkdownPreview.js
+++ src/components/MarkdownPreview.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import MarkdownIt from 'markdown-it'
 import hljs from 'highlightjs'
-import MarkdownMath from 'markdown-it-math'
+import katex from 'katex'
+import texmatch from 'markdown-it-texmath'
 import '../styles/markdown-preview.css'
 import '../styles/monokai.css'
+import 'katex/dist/katex.css'
 
 const renderMarkdown = (code) => {
   const md = new MarkdownIt({
@@ -19,9 +21,7 @@ const renderMarkdown = (code) => {
       }
       return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
     }
-  })//.use(MarkdownMath, {
- //   renderingOptions: { decimalMark: ',' }
-  //})
+  }).use(texmatch.use(katex), {delimiters:'dollars'})
   return {
     __html: md.render(code)
   }
